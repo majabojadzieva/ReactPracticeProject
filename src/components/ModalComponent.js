0b4cafc868a5f69670b90d355b9d3dc6f15fddff
Maja <majabojadzieva125s@gmail.com>
2022-08-04 11:05:06 +0200

    fetch PUT

--- src/components/ModalComponent.js
+++ src/components/ModalComponent.js
@@ -1,14 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
 function ModalComponent(props) {
   const [show, setShow] = useState(false);
 
+  const inputRef = useRef();
+
   const handleClose = () => setShow(false);
-  const handleShow = () => {
-    setShow(true);
-    // console.log(props.userId, props.userName);
+  const handleShow = () => setShow(true);
+  const submitHandler = () => {
+    async function editUser() {
+      const response = await fetch(
+        "https://62e27da2e8ad6b66d85cabf2.mockapi.io/api/v1/users/" +
+          props.userId,
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            name: inputRef.current.value,
+          }),
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+
+      const data = await response.json();
+      console.log(data.name);
+    }
+    editUser();
+
+    setShow(false);
   };
 
   return (
@@ -25,6 +47,7 @@ function ModalComponent(props) {
           <form className="d-flex justify-content-center">
             <label style={{ margin: "1rem" }}>New user name:</label>
             <input
+              ref={inputRef}
               style={{ margin: "1rem" }}
               placeholder={props.userName}
             ></input>
@@ -34,7 +57,7 @@ function ModalComponent(props) {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={submitHandler}>
             Save Changes
           </Button>
         </Modal.Footer>
