3803be240af0ab4c9ffd9d5e79cb8102d6f4bb80
Maja <majabojadzieva125s@gmail.com>
2022-08-23 17:45:05 +0200

    login validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,64 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
 export const Login = (props) => {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [emailWasTouched, setemailWasTouched] = useState(false);
+  const [passWasTouched, setpassWasTouched] = useState(false);
+
+  const passw =
+    /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*?]{6,}/g;
+
+  const enteredEmailIsValid = email.trim() !== "" && email.includes("@");
+  const enteredPassIsValid = password.match(passw);
+
+  const emailIsInvalid = !enteredEmailIsValid && emailWasTouched;
+  const passIsInvalid = !enteredPassIsValid && passWasTouched;
+
+  const handleOnChangePass = (inputText) => {
+    setPassword(inputText.target.value);
+  };
+
+  const handleOnChangeEmail = (inputText) => {
+    setEmail(inputText.target.value);
+  };
+
+  const handleOnFocusPass = () => {
+    setpassWasTouched(true);
+  };
+  const handleOnFocusEmail = () => {
+    setemailWasTouched(true);
+  };
+
+  const handleOnBlurPass = () => {
+    setpassWasTouched(false);
+  };
+
+  const handleOnBlurEmail = () => {
+    setemailWasTouched(false);
+  };
+
+  let formIsValid = false;
+
+  if (enteredEmailIsValid && enteredPassIsValid) {
+    formIsValid = true;
+  }
+
+  const validationMessagePass = (
+    <Form.Text style={{ color: "red" }}>
+      Password must contain at least 6 characters, one number, one uppercase
+      letter and one special character.
+    </Form.Text>
+  );
+
+  const validationMessageEmail = (
+    <Form.Text style={{ color: "red" }}>
+      Email must contain "@" character.
+    </Form.Text>
+  );
+
   return (
     <>
       <h1 className="m-5">Log in</h1>
@@ -15,24 +72,27 @@ export const Login = (props) => {
             <Form.Control
               type="email"
               placeholder="Enter email"
-              required
-              minLength="2"
+              value={email}
+              onChange={handleOnChangeEmail}
+              onFocus={handleOnFocusEmail}
+              onBlur={handleOnBlurEmail}
             />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
+            {emailIsInvalid && validationMessageEmail}
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control
               type="password"
               placeholder="Password"
-              required
-              minLength="2"
+              onChange={handleOnChangePass}
+              onFocus={handleOnFocusPass}
+              onBlur={handleOnBlurPass}
+              value={password}
             />
+            {passIsInvalid && validationMessagePass}
           </Form.Group>
 
-          <Button variant="dark" type="submit">
+          <Button variant="dark" type="submit" disabled={!formIsValid}>
             Log in
           </Button>
         </Form>
