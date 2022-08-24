5f8506492474ad508a49836cd7697f02efa20046
Maja <majabojadzieva125s@gmail.com>
2022-08-24 12:57:19 +0200

    password pattern

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,64 +1,7 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { useState } from "react";
 
 export const Login = (props) => {
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [emailWasTouched, setemailWasTouched] = useState(false);
-  const [passWasTouched, setpassWasTouched] = useState(false);
-
-  const passw =
-    /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*?]{6,}/g;
-
-  const enteredEmailIsValid = email.trim() !== "" && email.includes("@");
-  const enteredPassIsValid = password.match(passw);
-
-  const emailIsInvalid = !enteredEmailIsValid && emailWasTouched;
-  const passIsInvalid = !enteredPassIsValid && passWasTouched;
-
-  const handleOnChangePass = (inputText) => {
-    setPassword(inputText.target.value);
-  };
-
-  const handleOnChangeEmail = (inputText) => {
-    setEmail(inputText.target.value);
-  };
-
-  const handleOnFocusPass = () => {
-    setpassWasTouched(true);
-  };
-  const handleOnFocusEmail = () => {
-    setemailWasTouched(true);
-  };
-
-  const handleOnBlurPass = () => {
-    setpassWasTouched(false);
-  };
-
-  const handleOnBlurEmail = () => {
-    setemailWasTouched(false);
-  };
-
-  let formIsValid = false;
-
-  if (enteredEmailIsValid && enteredPassIsValid) {
-    formIsValid = true;
-  }
-
-  const validationMessagePass = (
-    <Form.Text style={{ color: "red" }}>
-      Password must contain at least 6 characters, one number, one uppercase
-      letter and one special character.
-    </Form.Text>
-  );
-
-  const validationMessageEmail = (
-    <Form.Text style={{ color: "red" }}>
-      Email must contain "@" character.
-    </Form.Text>
-  );
-
   return (
     <>
       <h1 className="m-5">Log in</h1>
@@ -72,27 +15,23 @@ export const Login = (props) => {
             <Form.Control
               type="email"
               placeholder="Enter email"
-              value={email}
-              onChange={handleOnChangeEmail}
-              onFocus={handleOnFocusEmail}
-              onBlur={handleOnBlurEmail}
+              required
+              minLength="2"
             />
-            {emailIsInvalid && validationMessageEmail}
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control
               type="password"
               placeholder="Password"
-              onChange={handleOnChangePass}
-              onFocus={handleOnFocusPass}
-              onBlur={handleOnBlurPass}
-              value={password}
+              pattern={
+                "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,}$"
+              }
+              title="Password must contain at least 8 characters, one number, one uppercase
+             letter and one special character."
             />
-            {passIsInvalid && validationMessagePass}
           </Form.Group>
-
-          <Button variant="dark" type="submit" disabled={!formIsValid}>
+          <Button variant="dark" type="submit">
             Log in
           </Button>
         </Form>
